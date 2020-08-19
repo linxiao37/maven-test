6774190d3ce3342ccbce54d2df37b467f9e2a137
xiaolin35 <xiaolin35@jd.com>
2020-08-19 11:03:36 +0800

    'init'

---
+++ src/main/java/com/alan/maven/test/maventest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.alan.maven.test.maventest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MavenTestApplication.class);
+    }
+
+}
---
+++ src/test/java/com/alan/maven/test/maventest/MavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.maven.test.maventest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
